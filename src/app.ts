88e66d3e93bd1d88bd6c3e8872e9f5f050030422
karolrezende <karolrds01@gmail.com>
2023-08-25 17:42:24 -0400

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import  express from "express"
 import { handleError } from "./error"
 import 'reflect-metadata'
+import { courseRoute } from "./routes/courses.routes"
 const app = express()
 
 app.use(express.json())
+
+app.use('/curso', courseRoute)
+
 app.use(handleError)
 export default app
